8166e47b74eff591941e973c1d6ecb5a0d633660
Ryder <ryderblue2@gmail.com>
2021-06-11 12:22:02 -0700

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,37 @@
-// build your `/api/projects` router here
+const express = require('express')
+const router = express.Router()
+const Projects = require('./model')
+
+router.get('/', async (req, res, next) => {
+    try {
+        const data = await Projects.getAll()
+        
+        const transformedData = data.map(x => {
+            const completed = () => {
+                if(x.project_completed == 1) {
+                    return 'true'
+                }
+                else {
+                    return 'false'
+                }
+            }
+            return {
+                project_id: x.project_id,
+                project_name: x.project_name,
+                project_description: x.project_description,
+                project_completed: completed
+            }
+        })
+        res.json(transformedData)
+        
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({message: err.message, stack: err.stack})
+})
+
+module.exports = router
